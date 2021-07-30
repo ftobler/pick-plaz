78a573d01015e221241fbdc42a3f436a8f6991c4
Nicolas Tobler <nitobler@gmail.com>
2021-07-30 23:48:21 +0200

    added mock api

--- python/bottle_svr.py
+++ python/bottle_svr.py
@@ -1,6 +1,9 @@
 #tornado needs this or it does not run
 import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+try:
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+except AttributeError:
+    print("Probably running on linux")
 
 from bottle import route, run, template, request, response, static_file
 
@@ -20,10 +23,43 @@ def home():
 def home(name):
     return static_file(name, root='web')
 
-@route('/api/<name>')
-def home(name):
-    return static_file(name, root='web/api')
-    
+# Use mock api if argument "mock" is passed
+import sys
+mock_api = sys.argv[1] == "mock" if len(sys.argv) > 1 else False
+if mock_api:
+    @route('/api/<name>')
+    def mock_api(name):
+        if name == "image.jpg":
+            import numpy as np
+            import cv2
+            img = np.random.uniform(0,255, size=(480,640)).astype(np.uint8)
+            status, encoded = cv2.imencode('.jpg', img)
+            if status:
+                jpg = encoded.tobytes()
+                response.set_header('Content-type', 'image/jpeg')
+                return jpg
+            else:
+                return "{}"
+        else:
+            return static_file(name, root='web/api')
+else:
+    @route('/api/<name>')
+    def raspberrypi_api(name):
+        # for now same as mock api
+        if name == "image.jpg":
+            import numpy as np
+            import cv2
+            img = np.random.uniform(0,255, size=(480,640)).astype(np.uint8)
+            status, encoded = cv2.imencode('.jpg', img)
+            if status:
+                jpg = encoded.tobytes()
+                response.set_header('Content-type', 'image/jpeg')
+                return jpg
+            else:
+                return "{}"
+        else:
+            return static_file(name, root='web/api')
+        
 @route('/parts/<name>')
 def home(name):
     return static_file(name, root='web/parts')
