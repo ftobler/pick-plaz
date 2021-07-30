1cc6eee7443b6cdea83e18dec197015c2ec1b8d8
Florin Tobler <florin.tobler@hotmail.com>
2021-07-31 01:45:34 +0200

    navigate

--- python/web/pickplaz.js
+++ python/web/pickplaz.js
@@ -6,17 +6,33 @@ function start() {
         el: "#app",
         data: {
             nav: {
-                page: 0
+                page: 2
             },
             db: {
 
+            },
+            canvas: {
+                ctx: null,
+                cursor_px: { x: 0, y: 0 },
+                cursor_mm: { x: 0, y: 0 },
+                drag_start_mm: { x: 0, y: 0},
+                drag: false,
+                pos_mm: { x: 200, y: 200 },
+                size: { x: 0, y: 0 },
+                zoom: 1.2,
             }
+        },
+        mounted() {
+            document.addEventListener('mousemove', this.mousemove)   
         },
         created() {
             this.poll()
+            var c = document.getElementById("canvas-view");
+            c.onmousewheel = this.mousewheel()
+            this.ctx = c.getContext('2d');
         },
         methods: {
-            poll: function() {
+            poll() {
                 ajax({
                     type: "GET",
                     dataType: "application/json",
@@ -24,6 +40,7 @@ function start() {
                     success: (data) => {
                         this.db = JSON.parse(data)
                         let i = 0
+                        this.draw_stuff()
                         /**for (const bom of this.db.bom) {
                             console.log(bom)
                             for (const id of bom.id) {
@@ -40,7 +57,75 @@ function start() {
                         }*/
                     },
                 })
-            }
+            },
+            px_to_mm(px) {
+                return this.px_to_mm_save(this.canvas.pos_mm, px)
+            },
+            px_to_mm_save(pos_mm, px) {
+                return {
+                    x: pos_mm.x + ((px.x - this.canvas.size.x/2) / this.canvas.zoom),
+                    y: pos_mm.y + ((px.y - this.canvas.size.y/2) / this.canvas.zoom)
+                }
+            },
+            show_coordinates(e) {
+                this.canvas.cursor_px.x = e.offsetX;
+                this.canvas.cursor_px.y = e.offsetY;
+                let mm = this.px_to_mm(this.canvas.cursor_px)
+                this.canvas.cursor_mm.x = Math.round(mm.x)
+                this.canvas.cursor_mm.y = Math.round(mm.y)
+            },
+            draw_stuff() {
+                var c = document.getElementById("canvas-view");
+                this.canvas.size.x = c.width
+                this.canvas.size.y = c.height
+                if (c == null) {
+                    return
+                }
+                let ctx = c.getContext('2d');
+                ctx.lineWidth = 1 / this.canvas.zoom
+                ctx.resetTransform()
+                ctx.clearRect(0,0, c.width, c.height)
+                ctx.translate(c.width / 2, c.height / 2)
+                ctx.scale(this.canvas.zoom, this.canvas.zoom)
+                ctx.translate(-this.canvas.pos_mm.x, -this.canvas.pos_mm.y)
+                ctx.strokeStyle = "white"
+                ctx.beginPath(); ctx.rect(0, 0, 400, 400); ctx.stroke();
+                ctx.scale(1, 1)
+            },
+            mousewheel(e) {
+                let fact = 1.1;
+                if (event.wheelDelta < 0) {
+                    fact = 1/fact;
+                }
+                this.canvas.zoom *= fact
+                this.draw_stuff()
+            },
+            mousedown(event) {
+                let canvas = document.getElementById("canvas-view");
+                this.canvas.drag = true
+                //this.canvas.drag_start_mm.x = this.canvas.pos_mm.x;
+                //this.canvas.drag_start_mm.y = this.canvas.pos_mm.y;
+                this.canvas.drag_start_mm = this.px_to_mm({
+                    x: event.pageX - canvas.offsetLeft,
+                    y: event.pageY - canvas.offsetTop
+                })
+            },
+            mouseup(event) {
+                this.canvas.drag = false
+            },
+            mousemove(event) {
+                if (this.canvas.drag == true) {
+                    let canvas = document.getElementById("canvas-view");
+                    let dragEnd_mmm = this.px_to_mm_save(this.canvas.drag_start_mm, {
+                        x: event.pageX - canvas.offsetLeft,
+                        y: event.pageY - canvas.offsetTop
+                    })
+                    this.canvas.dragEnd_mmm = dragEnd_mmm
+                    this.canvas.pos_mm.x = 2*this.canvas.drag_start_mm.x - dragEnd_mmm.x
+                    this.canvas.pos_mm.y = 2*this.canvas.drag_start_mm.y - dragEnd_mmm.y
+                    this.draw_stuff()
+                }
+            },
         },
         filters: {
             footprint_img_path: function(f) {
